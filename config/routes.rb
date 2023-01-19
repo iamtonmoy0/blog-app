68bc06478eb7e39337db91c6b849945aca6cefca
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-19 09:40:27 +0600

    post section added and routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts do
+  	resources :comments
+  end
 
  root 'posts#index'
+
 end
